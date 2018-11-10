11d88b0f1db0d730928ae9334f20a00ac578c550
grant smith <grantsmith1256@gmail.com>
2018-11-10 21:39:14 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,26 @@
+class TicTacToe
+  
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  
+  WIN_COMBINATIONS = [[0,1,2], [0,3,6],[3,4,5],[6,7,8], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+  
+  
+  def display_board
+    
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  
+  def input_to_index(user_input)
+    user_input = gets.chomp
+    user_input.to_i(user_input)
+    user_input - 1
+  end
+  
+end
